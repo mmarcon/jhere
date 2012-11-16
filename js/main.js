94fd89aa766954a3faaac4ace1b0df2b12ec6abf
Massimiliano Marcon <me@marcon.me>
2012-11-16 16:28:53 +0100

    analytics tracking for outbound links

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(function(){
     $('.email').attr('href', 'mailto:max@jhere.net').text('max@jhere.net');
+    $('a').on('click', function(e){
+        if(!_gaq) {
+            return true;
+        }
+        e.preventDefault();
+        var url = $(this).attr("href");
+        if (e.currentTarget.host != window.location.host) {
+            try {
+                _gaq.push(['_trackEvent', 'Outbound Links', e.currentTarget.host, url, 0]);
+                setTimeout('document.location = "' + url + '"', 100);
+            } catch(err){
+                document.location = url;
+            }
+        }
+    });
 });
 $(window).on('load', function(){
     var header = $('header'),
