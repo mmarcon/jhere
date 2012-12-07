1f934a9e3f710c725e9af61ab1ac595e592d160d
Massimiliano Marcon <me@marcon.me>
2012-12-07 16:00:33 +0100

    Improvements in implementation

--- src/extensions/advancedmarkers.js
+++ src/extensions/advancedmarkers.js
@@ -21,17 +21,21 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 ;(function($){
-    var _ns, marker, M, _default = {
+    var UNGROUPED = 'ungrouped',
+        _ns, marker, M, G, _default = {
         marker: {
             text: '',
             textColor: '#333333',
             fill: '#ff6347',
             stroke: '#333333',
             shape: 'balloon',
-            icon: null,
-            group: null
+            icon: undefined,
+            group: UNGROUPED
         }
-    };
+    },
+    groups = {};
+
+
 
     /*http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery*/
     function hashCode(string){
@@ -48,12 +52,44 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         return hash;
     }
 
+    function jHEREMarkerGroup(jslaContainer, name, map){
+        this.container = jslaContainer;
+        this.name = name;
+        this.map = map;
+        this.markers = [];
+        this.visible = true;
+    }
+
+    G = jHEREMarkerGroup.prototype;
+
+    G.addMarker = function(marker){
+        this.markers.push(marker);
+    };
+
+    G.hide = function(){
+        if(this.visible) {
+            this.map.objects.remove(this.container);
+        }
+    };
+
+    G.show = function(){
+        if(!this.visible) {
+            this.map.objects.show(this.container);
+        }
+    };
+
+    jHEREMarkerGroup.getGroup = function(group){
+        group = group || UNGROUPED;
+        return groups[group];
+    };
+
     function jHEREMarker(jslaMarker, config){
         this.marker = jslaMarker;
         this.selected = false;
         this.listeners = {};
         this.element = config.element;
         this.map = config.map;
+        this.group = config.group;
     }
 
     M = jHEREMarker.prototype;
@@ -76,9 +112,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             });
             /*
              When the event listener is called then
-             the context is the DOM element containing the map.
+             the context is the marker.
             */
-            callback.call(this.element, e);
+            callback.call(this, e);
         };
         b = $.proxy(_callback, this);
         this.listeners[event][''+hashCode(callback)] = b;
@@ -94,11 +130,15 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             });
             return;
         }
-        this.marker.removeListener(event, l[event][''+hashCode(callback)]);
+        this.marker.removeListener(event, l[event]['' + hashCode(callback)]);
     };
 
     M.remove = function(){
+        groups[this.group].objects.remove(this.marker);
+    };
 
+    M.add = function(){
+        groups[this.group].objects.add(this.marker);
     };
 
     /*
@@ -111,43 +151,44 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         - draggable marker
     */
 
+    function init(){
+        if(_ns) {
+            return;
+        }
+        _ns = nokia.maps;
+    }
+
     marker = function(position, markerOptions, closure) {
         var marker;
+
+        init();
+        if($.isFunction(markerOptions)) {
+            closure = markerOptions;
+            markerOptions = {};
+        }
 
         markerOptions = $.extend({}, _default.marker, markerOptions);
         /*Normalize settings*/
         markerOptions.textPen = markerOptions.textPen || {strokeColor: markerOptions.textColor};
         markerOptions.pen = markerOptions.pen || {strokeColor: markerOptions.stroke};
         markerOptions.brush = markerOptions.brush || {color: markerOptions.fill};
-        markerOptions.eventListener = markerListeners;
+
+        /*Create group if not existing already*/
+        if(!groups[markerOptions.group]){
+            groups[markerOptions.group] = new jHEREMarkerGroup(_ns.map.Container(), markerOptions.group, this.map);
+            this.map.objects.add(groups[markerOptions.group].container);
+            markerOptions.group = groups[markerOptions.group];
+        }
 
         if (markerOptions.icon) {
-            marker = new jHEREMarker(new _ns.map.Marker(position, markerOptions));
+            marker = new jHEREMarker(new _ns.map.Marker(position, markerOptions), markerOptions);
         } else {
-            marker = new jHEREMarker(new _ns.map.StandardMarker(position, markerOptions));
+            marker = new jHEREMarker(new _ns.map.StandardMarker(position, markerOptions), markerOptions);
         }
 
+        marker.add();
         closure.call(this.element, marker);
     };
-
-    function triggerEvent(event) {
-        var handler = event.target[event.type];
-        if (isFunction(handler)) {
-            var e = $.Event(event.type, {
-                originalEvent: event,
-                geo: {
-                    latitude: event.target.coordinate.latitude,
-                    longitude: event.target.coordinate.longitude
-                },
-                target: event.target
-            });
-            /*
-             When the event listener is called then
-             the context is the DOM element containing the map.
-            */
-            handler.call(this.element, e);
-        }
-    }
 
     function isFunction(fn) {
         return typeof fn === 'function';
