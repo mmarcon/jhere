88ec667a3bc585c577fe38f9f4260976258c0d0d
Massimiliano Marcon <me@marcon.me>
2015-08-18 14:53:51 +0200

    Adds center method.
    And let the async issues begin.

--- src/modules/core.js
+++ src/modules/core.js
@@ -1,7 +1,11 @@
 /* globals H: true */
+require("babel/polyfill");
+
 import Loader from './loader';
 import * as config from './config';
 import {extend} from './utils';
+
+new Promise();
 
 
 const w = window;
@@ -10,12 +14,12 @@ const d = document;
 const constructor = w.jHERE = function jHERE(element, options){
     this.el = element;
     this.options = extend(config.defaults, options);
-    this.init();
+    this._init();
 };
 
 const JH = constructor.prototype;
 
-JH.init = function(){
+JH._init = function(){
     const self = this;
     const classList = self.el.classList;
     const apiLoader = new Loader();
@@ -25,10 +29,10 @@ JH.init = function(){
         return;
     }
     self.el.classList.add(config.lib);
-    apiLoader.require(modules, d.querySelector('script[src*="jhere"]'), () => self.makemap());
+    apiLoader.require(modules, d.querySelector('script[src*="jhere"]'), () => self._makemap());
 };
 
-JH.makemap = function(){
+JH._makemap = function(){
     const self = this;
     self.platform = new H.service.Platform({
         app_id: self.options.credentials.appId,
@@ -36,4 +40,9 @@ JH.makemap = function(){
     });
     self.layers = self.platform.createDefaultLayers();
     self.map = new H.Map(self.el, self.layers.normal.map, self.options);
+};
+
+JH.center = function(newCenter, animate){
+    this.map.setCenter(newCenter, animate);
+    return this;
 };
